ee044c6b638d57b12a7c3df45b0c79dc953ae96e
Nishthaa-Nanda <nishthananda100@gmail.com>
2025-07-19 16:12:32 +0530

    modified app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,22 @@
 
+import { version } from 'react';
 import './App.css';
 import Video  from './videos';
 
 
 function App() { 
+  let obj={
+    verified:true,
+    channel:'codewithmik',
+    title:'tutorial1',
+    views:'90K',
+    time:'3 years'
+  }
   return (
     <>
     <div className="App">
      <Video  verified={true} channel='coder help'title='tutorial' views='88K' time ='2 years'></Video>
-     <Video></Video>
+     <Video>{...obj}</Video>
      </div>
     </>
   );
